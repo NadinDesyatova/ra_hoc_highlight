d50cb83d948fe2511de6ff819af1ffb910b26a6a
NadinDesyatova <nadindinina@yandex.ru>
2025-04-18 11:47:20 +0300

    necessary adjustments have been made

--- src/ui/Article.tsx
+++ src/ui/Article.tsx
@@ -2,10 +2,10 @@ import { PostPropsType } from "../common/page-components-types";
 
 
 export function Article(props: PostPropsType) {
-    return (
-        <div className="item item-article">
-            <h3><a href="#">{props.title}</a></h3>
-            <p className="views">Прочтений: {props.views}</p>
-        </div>
-    )
+  return (
+    <div className="item item-article">
+      <h3><a href="#">{props.title}</a></h3>
+      <p className="views">Прочтений: {props.views}</p>
+    </div>
+  )
 };
